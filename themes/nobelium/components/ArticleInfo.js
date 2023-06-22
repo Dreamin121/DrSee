382eb486c6382ff174f468018f7dff76c2ee1b26
Dreamin121 <92585241+Dreamin121@users.noreply.github.com>
2023-06-22 19:43:41 +0800

    Update ArticleInfo.js

--- themes/nobelium/components/ArticleInfo.js
+++ themes/nobelium/components/ArticleInfo.js
@@ -25,7 +25,7 @@ export const ArticleInfo = (props) => {
                   alt={BLOG.author}
                   width={24}
                   height={24}
-                  src={`https://www.xmelon.cafe/favicon.ico`}
+                  src="https://www.xmelon.cafe/favicon.ico"
                   className="rounded-full"
                 />
                 <p className="ml-2 md:block">{BLOG.author}</p>
