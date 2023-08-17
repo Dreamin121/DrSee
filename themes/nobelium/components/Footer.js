3665b74a99f4192445751f01510904d245fadb62
Dreamin <92585241+Dreamin121@users.noreply.github.com>
2023-08-17 21:51:35 +0800

    Update Footer.js

--- themes/nobelium/components/Footer.js
+++ themes/nobelium/components/Footer.js
@@ -1,49 +1,53 @@
-import BLOG from '@/blog.config'
-import DarkModeButton from '@/components/DarkModeButton'
-import Vercel from '@/components/Vercel'
+import BLOG from '@/blog.config';
+import DarkModeButton from '@/components/DarkModeButton';
+import Vercel from '@/components/Vercel';
 import Head from 'next/head';
 import { useEffect } from 'react';
-
+import hig from '@/Hig';
 
 export const Footer = (props) => {
-useEffect(() => {
+  useEffect(() => {
     // 在这里调用hig()函数
     hig();
   }, []);
 
-  const d = new Date()
-  const currentYear = d.getFullYear()
-  const { post } = props
-  const fullWidth = post?.fullWidth ?? false
+  const d = new Date();
+  const currentYear = d.getFullYear();
+  const { post } = props;
+  const fullWidth = post?.fullWidth ?? false;
 
   const copyrightDate = (function() {
     if (Number.isInteger(BLOG.SINCE) && BLOG.SINCE < currentYear) {
-      return BLOG.SINCE + '-' + currentYear
+      return BLOG.SINCE + '-' + currentYear;
     }
-    return currentYear
-  })()
+    return currentYear;
+  })();
 
-  return <footer
-     className={`z-10 relative mt-6 flex-shrink-0 m-auto w-full text-gray-500 dark:text-gray-400 transition-all ${
-       !fullWidth ? 'max-w-2xl px-4' : 'px-4 md:px-24'
-     }`}
-   >
+  return (
+    <footer
+      className={`z-10 relative mt-6 flex-shrink-0 m-auto w-full text-gray-500 dark:text-gray-400 transition-all ${
+        !fullWidth ? 'max-w-2xl px-4' : 'px-4 md:px-24'
+      }`}
+    >
       <Head>
         <script src="https://dl.lancdn.com/landian/dev/high/src/high.js" />
       </Head>
-     <DarkModeButton className='text-center pt-4'/>
+      <DarkModeButton className='text-center pt-4'/>
       <p align="center" >   
         <img src="https://profile-counter.glitch.me/xmelon.cafe/count.svg" />  
       </p>
-     <hr className="border-gray-200 dark:border-gray-600" />
-     <div className="my-4 text-sm leading-6">
-       <div className="flex align-baseline justify-between flex-wrap">
-         <p>
-           © {BLOG.AUTHOR} {copyrightDate}
-         </p>
-           <button type="button" className="btn btn-link" onClick={hig}>High一下</button>
-         <Vercel />
-       </div>
-     </div>
-   </footer>
-}
+      <hr className="border-gray-200 dark:border-gray-600" />
+      <div className="my-4 text-sm leading-6">
+        <div className="flex align-baseline justify-between flex-wrap">
+          <p>
+            © {BLOG.AUTHOR} {copyrightDate}
+          </p>
+          <button type="button" className="btn btn-link" onClick={hig}>
+            High一下
+          </button>
+          <Vercel />
+        </div>
+      </div>
+    </footer>
+  );
+};
